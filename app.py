8eba1058d651b9747ddaf97b8df17174a0a2f8af
JERRYxPROOO <273725@student.pwr.edu.pl>
2025-12-05 00:49:09 +0100

    popraeki w kodzie

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ SQL_USERNAME = os.getenv("AZURE_USER")
 SQL_PASSWORD = os.getenv("AZURE_PASS")
 SQL_PORT = os.getenv("AZURE_PORT", "1433")
 
+
 CONN_STR = (
     f"DRIVER={{ODBC Driver 18 for SQL Server}};"
     f"SERVER={SQL_SERVER},{SQL_PORT};"
